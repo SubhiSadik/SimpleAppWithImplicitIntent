963a4309a398fc15b3a6c04fb03605811b1aca67
SubhiSadik <solaimansadik19@gmail.com>
2024-05-29 20:09:24 +0600

    first commit

---
+++ app/src/main/java/bd/edu/seu/solaiman/implicitintent/MainActivity.kt
@@ -0,0 +1,34 @@
+package bd.edu.seu.solaiman.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
+import android.provider.MediaStore
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val webButton= findViewById<CardView>(R.id.cardWeb)
+        val cameraButton= findViewById<CardView>(R.id.cardCamera)
+
+        webButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://pi.ai/discover")
+            startActivity(intent)
+        }
+
+        cameraButton.setOnClickListener{
+            val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+    }
+}
